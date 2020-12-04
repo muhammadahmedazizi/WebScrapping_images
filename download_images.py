2e25be846f718c1dd00b1c18e80c648cfa48bd10
Muhammad Ahmed <muhammad.ahmed.azizi@gmail.com>
2020-12-04 17:48:12 +0500

    The script download images from the given links

---
+++ download_images.py
@@ -0,0 +1,30 @@
+import requests
+import time 
+
+
+
+Image_links_list = ['https://upload.wikimedia.org/wikipedia/commons/e/ee/Wilcox.jpg']
+
+
+def save_images (url_list):
+    ind = 1
+    for pic_url in url_list:
+        picName = "pic" + str(ind)+".jpg"
+        with open(picName, 'wb') as handle:
+                
+
+                response = requests.get(pic_url, stream=True)
+
+                if not response.ok:
+                    print (response)
+
+                for block in response.iter_content(1024):
+                    if not block:
+                        break
+
+                    handle.write(block)
+                    ind = ind + 1
+
+
+
+save_images(Image_links_list)
